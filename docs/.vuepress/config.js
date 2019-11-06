3823633fb8255543d658e96ced8229621bb0d9ab
hzjanger <1305696249@qq.com>
2019-11-06 22:26:27 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    base: '/reptile/',
+    title: 'python爬虫学习笔记',
+    description: '今天很残忍,明天很残酷,但后天很美好',
+    head: [
+        ['link', { rel: 'icon', href: `/favicon.png` }],
+    ],
+    themeConfig: {
+        nav: [
+            { text: '我的博客', link: 'https://hzjanger.github.io/', target: false}
+            // { text: 'Guide', link: '/guide/' },
+            // { text: 'External', link: 'https://google.com' },
+        ],
+        // 将会自动在每个页面的导航栏生成生成一个 GitHub 链接，以及在页面的底部生成一个 "Edit this page" 链接
+        //假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'hzjanger/reptile',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 默认为 "Edit this page"
+        editLinkText: '在 GitHub 上编辑此页',
+        //每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+        lastUpdated: '上次更新',
+        sidebar: [
+            {
+                title: '爬虫基础',
+                children: [
+                    '爬虫基础/http基本原理'
+                ]
+            },
+            {
+                title: '常见库的使用',
+                children: [
+                    '常见库的使用/requests'
+                ]
+            },
+            {
+                title: '案例',
+                children: [
+                    '案例/猫眼电影'
+                ]
+            }
+        ]
+    }
+};
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+    home: true
+    heroImage: /logo.jpg
+    actionText: 开始阅读
+    actionLink: /爬虫基础/http基本原理.html
+    features:
+    - title: 踩坑
+      details: 记录学习过程中踩过的坑
+    - title: 总结
+      details: 将自己平时的学习进行总结归纳
+    - title: 完善
+      details: 刚开始学，还有待完善
+    footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+<!-- # 其他学习笔记
+
+- [java学习笔记](https://hzjanger.github.io/SpringInActionStudy/) -->
